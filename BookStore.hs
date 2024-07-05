0717ad8f5cf51d099254cea279bcae45f4860d96
Arin Salabsi <69157252+arrrin@users.noreply.github.com>
2024-07-05 11:17:55 +0700

    Update BookStore.hs

--- BookStore.hs
+++ BookStore.hs
@@ -1,12 +1,35 @@
 data BookInfo = Book Int String [String]
   deriving (Show)
 
+-- behind = is contructor
 data MagazineInfo = Magazine Int String [String]
   deriving (Show)
 
-data Customer = Customer Int String
+type CustomerID = Int
+
+type ReviewBody = String
+
+type CardHolder = String
+
+type CardNumber = String
+
+type Address = [String]
+
+data BookReview = BookReview BookInfo CustomerID ReviewBody
+
+type BookRecord = (BookInfo, BookReview)
+
+data BillingInfo
+  = CreditCard CardNumber CardHolder Address
+  | CashOnDelivery
+  | Invoice CustomerID
   deriving (Show)
 
-data BookReview = BookReview BookInfo Customer String
-
 myInfo = Book 123 "Practicing" ["Arin"]
+
+-- Credit card
+myBill = CreditCard "123" "Arin" ["Thailand"]
+
+nyCash = CashOnDelivery
+
+invoice = Invoice 123
